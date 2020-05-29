b46b9104b522ba4d01a997d75db7e00422b1a280
Everton Constantino <evertonwilliam@gmail.com>
2020-05-29 19:04:15 -0300

    Version 1.0

---
+++ MouseMove/FPrincipal.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace MouseMove
+{
+    partial class FPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BExecute = new System.Windows.Forms.Button();
+            this.TQtdeVezes = new System.Windows.Forms.TextBox();
+            this.LQuantidadeVezes = new System.Windows.Forms.Label();
+            this.SStatus = new System.Windows.Forms.StatusStrip();
+            this.SStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.SStatusTecla = new System.Windows.Forms.ToolStripStatusLabel();
+            this.BExit = new System.Windows.Forms.Button();
+            this.SStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BExecute
+            // 
+            this.BExecute.Location = new System.Drawing.Point(448, 13);
+            this.BExecute.Name = "BExecute";
+            this.BExecute.Size = new System.Drawing.Size(150, 85);
+            this.BExecute.TabIndex = 0;
+            this.BExecute.Text = "&Execute";
+            this.BExecute.UseVisualStyleBackColor = true;
+            this.BExecute.Click += new System.EventHandler(this.BExecute_Click);
+            // 
+            // TQtdeVezes
+            // 
+            this.TQtdeVezes.Location = new System.Drawing.Point(12, 35);
+            this.TQtdeVezes.Name = "TQtdeVezes";
+            this.TQtdeVezes.Size = new System.Drawing.Size(83, 22);
+            this.TQtdeVezes.TabIndex = 1;
+            this.TQtdeVezes.Text = "1";
+            this.TQtdeVezes.TextChanged += new System.EventHandler(this.TQtdeVezes_TextChanged);
+            // 
+            // LQuantidadeVezes
+            // 
+            this.LQuantidadeVezes.AutoSize = true;
+            this.LQuantidadeVezes.Location = new System.Drawing.Point(13, 13);
+            this.LQuantidadeVezes.Name = "LQuantidadeVezes";
+            this.LQuantidadeVezes.Size = new System.Drawing.Size(82, 17);
+            this.LQuantidadeVezes.TabIndex = 2;
+            this.LQuantidadeVezes.Text = "Qtde Vezes";
+            // 
+            // SStatus
+            // 
+            this.SStatus.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.SStatus.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SStatusLabel,
+            this.SStatusTecla});
+            this.SStatus.Location = new System.Drawing.Point(0, 205);
+            this.SStatus.Name = "SStatus";
+            this.SStatus.Size = new System.Drawing.Size(620, 26);
+            this.SStatus.TabIndex = 5;
+            this.SStatus.Text = "SStatus";
+            // 
+            // SStatusLabel
+            // 
+            this.SStatusLabel.Name = "SStatusLabel";
+            this.SStatusLabel.Size = new System.Drawing.Size(105, 20);
+            this.SStatusLabel.Text = "Status: Parado.";
+            // 
+            // SStatusTecla
+            // 
+            this.SStatusTecla.Name = "SStatusTecla";
+            this.SStatusTecla.Size = new System.Drawing.Size(0, 20);
+            // 
+            // BExit
+            // 
+            this.BExit.Location = new System.Drawing.Point(448, 105);
+            this.BExit.Name = "BExit";
+            this.BExit.Size = new System.Drawing.Size(150, 94);
+            this.BExit.TabIndex = 4;
+            this.BExit.Text = "&Exit";
+            this.BExit.UseVisualStyleBackColor = true;
+            this.BExit.Click += new System.EventHandler(this.BExit_Click);
+            // 
+            // FPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 231);
+            this.Controls.Add(this.SStatus);
+            this.Controls.Add(this.BExit);
+            this.Controls.Add(this.LQuantidadeVezes);
+            this.Controls.Add(this.TQtdeVezes);
+            this.Controls.Add(this.BExecute);
+            this.MaximizeBox = false;
+            this.Name = "FPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mouse Move 1.0";
+            this.SStatus.ResumeLayout(false);
+            this.SStatus.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BExecute;
+        private System.Windows.Forms.TextBox TQtdeVezes;
+        private System.Windows.Forms.Label LQuantidadeVezes;
+        private System.Windows.Forms.StatusStrip SStatus;
+        private System.Windows.Forms.ToolStripStatusLabel SStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel SStatusTecla;
+        private System.Windows.Forms.Button BExit;
+    }
+}
+
---
+++ MouseMove/FPrincipal.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MouseMove
+{
+    public partial class FPrincipal : Form
+    {
+        /*
+         * FUNCAO QUE IDENTIFICA UMA TECLA PRESSIONADA FORA DA GUI
+         */
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern short GetAsyncKeyState(int vKey);
+
+        /*
+         * FUNCAO QUE MOVE O CURSOR NA TELA DO WINDOWS
+         * RECEBE LOCAL X E LOCAL Y
+         */
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int x, int y);
+
+        /* FUNCAO QUE APLICA EVENTO NO MOUSE
+         * RECEBE FLAGS, X, Y, BUTONS E INFOS
+         */
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        /*
+         * CONSTANTES QUE RETRATAM AS ACOES DO MOUSE
+         */
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        /*
+         * CONSTANTES QUE RETRATAM AS ACOES DO TECLADO
+         */
+        private const int VK_ESCAPE = 0x1B;    // ESC
+
+        /*
+         * DECLARAÇÃO DAS VARIÁVEIS DO SISTEMA
+         */
+        private Thread initRobo;
+        private Thread listenKeyPress;
+
+        /*
+         * INICIO DA APLICAÇÃO
+         */
+
+        public FPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void InitProcess()
+        {
+            try
+            {
+                int vezes = int.Parse(TQtdeVezes.Text);
+                int i = 0;
+
+                while (i < vezes)
+                {
+                    SStatusLabel.Text = "Status: Rodando... " + i.ToString();
+
+                    Thread.Sleep(1000);
+
+                    Cursor.Position = new Point(400, 400);
+
+                    mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+
+                    Thread.Sleep(1000);
+
+                    Cursor.Position = new Point(370, 465);
+
+                    Thread.Sleep(1000);
+
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+                    i++;
+
+                }
+                SStatusLabel.Text = "Status: Parado";
+                initRobo.Abort();
+            }
+            catch (Exception ex)
+            {
+                SStatusLabel.Text = ex.Message;
+            }
+
+        }
+
+        private void ListenKeyPressed()
+        {
+            SStatusTecla.Text = "Pressione ESC, para parar!";
+            while (true)
+            {
+                short keyState = GetAsyncKeyState(VK_ESCAPE);
+                //bool escIsPressed = ((keyState >> 15) & 0x0001) == 0x0001;
+                bool escUnProcessedPress = ((keyState >> 0) & 0x0001) == 0x0001;
+
+                if (escUnProcessedPress || initRobo.ThreadState == ThreadState.Stopped)
+                {
+                    break;
+                }
+            }
+            SStatusTecla.Text = "";
+            SStatusLabel.Text = "Status: Parado";
+            initRobo.Abort();
+        }
+
+        /*
+         * ACTIONS
+         */
+
+        private void BExecute_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                initRobo = new Thread(InitProcess);
+                initRobo.Start();
+
+                listenKeyPress = new Thread(ListenKeyPressed);
+                listenKeyPress.Start();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("A quantidade deve ser numérica");
+            }
+        }
+
+        private void BExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void TQtdeVezes_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int vezes = int.Parse(TQtdeVezes.Text);
+                if(vezes <= 0)
+                {
+                    MessageBox.Show("O valor é menor ou igual a zero");
+                    TQtdeVezes.Text = "1";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("O valor não é numérico");
+                TQtdeVezes.Text = "1";
+            }
+
+
+        }
+    }
+}
